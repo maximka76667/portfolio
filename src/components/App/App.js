832f6dc539f450c52066197e44587f9055bee44a
Maxim <74974283+maximka76667@users.noreply.github.com>
2022-03-02 12:22:04 +0100

    projects styles

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -14,6 +14,7 @@ import Footer from '../Footer/Footer';
 
 function App() {
   const [articles, setArticles] = React.useState([]);
+  const [projects, setProjects] = React.useState([]);
 
   React.useEffect(() => {
     setArticles([
@@ -42,6 +43,35 @@ function App() {
         content: '<h2>Travel</h2><p>Text</p>',
       },
     ].reverse());
+    setProjects([
+      {
+        _id: 321,
+        name: 'Article',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit consequatur ducimus at ut illum quaerat omnis, culpa beatae? Esse dignissimos unde quasi. Eveniet aspernatur provident qui ratione facilis dolore esse?',
+        img: 'https://media.istockphoto.com/photos/young-woman-reading-the-news-on-a-modern-tablet-computer-while-in-picture-id1177502660?b=1&k=20&m=1177502660&s=170667a&w=0&h=ROub8oDtheyx3xtHMjtU2FV6ZC7g2iSGX2qxme6388w=',
+        link: 'article',
+        linkName: 'See project',
+        repoLink: '',
+      },
+      {
+        _id: 105,
+        name: 'New article',
+        description: 'Description new article',
+        img: 'https://thumbs.dreamstime.com/z/article-reading-16383133.jpg',
+        link: 'new-article',
+        linkName: 'See project',
+        repoLink: '',
+      },
+      {
+        id: 110,
+        name: 'Barcelona',
+        description: 'My travel to Barcelona',
+        img: 'https://lh3.googleusercontent.com/aopfdWgAbydWD1kYlBMTM5wlVKlzU-lyTIyoDq9csvqLxC69Pw0Ba7cJnq-UShsZ2buIAHU_g3jX_ZoawyvaGft7oXhaCYr9Bb9gryHnmmtzrL0Ssb0pil3v0VXXRb4_c9bZLS_F1yR1I2KQGsv0IQwIXbyAMr5SPylJJsQJHgKZUVo8Yib3_NTrzTwmtbG1XH4Z2A5vMiccVAM9-YpGc4dp5nYTXb9IEjj6il8WtNVUrqjiXpdAI8EM6SX2pIeg_qiciqg-TObG1eHBKIzxELMKtGgXOXvqCzvIzpT15xczALsSmJsQTCE3NfLt9iiZJdZYOGC872Ohdpi5j4QSSvZQAaRahfW_3rXu5nwnMqWQ3H02H3QuyLKnGOYEqU7Z16EQeZdxiV6EVsYUKPkXYCRrmtkGyDcKZ0_zkwXEPLYXcTsX0Tb1_wQjeEOA5PmT9PFABP3tX0v-bcZ_oly3kOP2atOIWcnMstu28iYLcXYRKjKf4e1BcLAhcc4pmdEd63jAD_8gYSocCgqJ0c6VXKQa3paQfSCkJrjjDg4wBKFAu4y-IbF1fknGH8ZNDGqhdBzn2r4k38sW2MjEEg8XK8diDQW4utHhbIOh79tZmMk59HaiH6UZ5AG3g8QbiEi-Gacll0qdcjfwGgvB5ycPRwhOyfh3gU8fa3WXpNPa-4glOI30FjAUsoJacmWkGjT9hs-SxEJFD3e8_Rse3bkr3b1l=w507-h898-no?authuser=1',
+        link: 'barcelona',
+        linkName: 'See project',
+        repoLink: '',
+      },
+    ].reverse());
   }, []);
 
   return (
@@ -51,7 +81,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
+          <Route path="/projects" element={<Projects projects={projects} />} />
           <Route path="/blog">
             <Route path="/blog/" element={<Blog articles={articles} />} />
             <Route path="/blog/article/:linkName" element={<Article articles={articles} />} />
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,16 +1,48 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import './Projects.css';
+import PropTypes from 'prop-types';
 
-function Projects() {
+function Projects(props) {
+  const { projects } = props;
+
   return (
     <div className="projects">
       <Helmet>
         <title>Projects</title>
       </Helmet>
       <h1 className="projects__heading">Projects</h1>
+      <section className="projects__list">
+        {
+          projects.map((project) => (
+            <article className="project">
+              <div className="project__info">
+                <h2 className="project__name">{project.name}</h2>
+                <p className="project__description">{project.description}</p>
+                <div className="project__links">
+                  <a href={project.link} className="project__link">{project.linkName}</a>
+                  <a href={project.repoLink} className="project__link project__repo-link">See GitHub repository</a>
+                </div>
+              </div>
+              <img className="project__img" src={project.img} alt={project.name} />
+            </article>
+          ))
+        }
+      </section>
     </div>
   );
 }
+
+Projects.propTypes = {
+  projects: PropTypes.arrayOf(PropTypes.shape({
+    _id: PropTypes.number,
+    name: PropTypes.string,
+    description: PropTypes.string,
+    img: PropTypes.string,
+    link: PropTypes.string,
+    linkName: PropTypes.string,
+    repoLink: PropTypes.string,
+  })).isRequired,
+};
 
 export default Projects;
