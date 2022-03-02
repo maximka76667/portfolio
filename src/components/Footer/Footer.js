f0730bf660d4fa7bd9260dd8aaa22488ab09d9f9
Maxim <74974283+maximka76667@users.noreply.github.com>
2022-03-02 12:38:33 +0100

    change main background gradient

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -16,17 +16,17 @@ function Footer() {
       <ul className="footer__menu">
         <li className="footer__menu-item">
           <a href="https://www.linkedin.com/in/maxim-grivennyy-ba9a31231" className="footer__menu-link">
-            <img src={logoLinkedin} alt="" />
+            <img className="footer__menu-icon" src={logoLinkedin} alt="" />
           </a>
         </li>
         <li className="footer__menu-item">
           <a href="https://www.instagram.com/maximka76667" className="footer__menu-link">
-            <img src={logoInstagram} alt="" />
+            <img className="footer__menu-icon" src={logoInstagram} alt="" />
           </a>
         </li>
         <li className="footer__menu-item">
           <a href="https://vk.com/maximgriven" className="footer__menu-link">
-            <img src={logoVk} alt="" />
+            <img className="footer__menu-icon" src={logoVk} alt="" />
           </a>
         </li>
       </ul>
