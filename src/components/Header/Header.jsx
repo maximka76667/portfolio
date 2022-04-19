c1355daf9775b4091f2456c94fece5f458bcf630
Maxim <74974283+maximka76667@users.noreply.github.com>
2022-04-19 11:19:12 +0200

    direct import of Nav to Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Header.css';
-import { Nav } from '../index';
+import Nav from '../Nav/Nav';
 
 function Header() {
   return (
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,3 @@
 export { default as Header } from './Header/Header';
 export { default as Footer } from './Footer/Footer';
-export { default as Nav } from './Nav/Nav';
 export { default as Project } from './Project/Project';
