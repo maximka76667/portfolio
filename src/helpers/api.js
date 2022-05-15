9d97e3aecef42ec41caafbe3a1baa726cf9d181e
Maxim <74974283+maximka76667@users.noreply.github.com>
2022-05-15 19:30:03 +0200

    fix link

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -2,7 +2,7 @@ const projects = [
   {
     name: 'JWT Generator',
     img: 'https://imgur.com/rA1X9py.png',
-    link: 'maximka76667.github.io/jwt-generator',
+    link: 'https://maximka76667.github.io/jwt-generator',
     repoLink: 'https://github.com/maximka76667/jwt-generator',
     color: '#000000',
     bgColor: '#f7f7f7',
