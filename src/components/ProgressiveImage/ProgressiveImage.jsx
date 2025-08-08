f005c35908a575f8e7bfc2924af203005daf9f52
Maxim <74974283+maximka76667@users.noreply.github.com>
2025-08-08 23:30:57 +0500

    fix progressive image component

--- src/components/ProgressiveImage/ProgressiveImage.jsx
+++ src/components/ProgressiveImage/ProgressiveImage.jsx
@@ -3,13 +3,14 @@ import PropTypes from 'prop-types';
 
 const ProgressiveImage = ({ src, href, alt, children, ...props }) => {
   const placeholderSrc = `reduced-${src.slice(0, -4)}.jpg`;
+
   const [imgSrc, setImgSrc] = useState(placeholderSrc || src);
   const customClass =
     placeholderSrc && imgSrc === placeholderSrc ? 'loading' : 'loaded';
 
   useEffect(() => {
     const img = new Image();
-    img.src = src;
+    img.src = process.env.PUBLIC_URL + '/images/' + src;
     img.onload = () => {
       setImgSrc(src);
     };
@@ -19,7 +20,7 @@ const ProgressiveImage = ({ src, href, alt, children, ...props }) => {
     <a
       href={href}
       style={{
-        backgroundImage: `url(${process.env.PUBLIC_URL + '/images/' + src})`
+        backgroundImage: `url(${process.env.PUBLIC_URL + '/images/' + imgSrc})`
       }}
       alt={alt || ''}
       className={`project__img-link ${customClass}`}
